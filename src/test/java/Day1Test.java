57d55fb49d779b5697c3b73e7985e513f325cb8a
Cyril Houillon <houillon.cyril@gmail.com>
2023-12-01 23:37:37 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/test/java/Day1Test.java

--- src/test/java/Day1Test.java
+++ src/test/java/Day1Test.java
@@ -1,45 +1,68 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class Day1Test {
 
-    //1abc2
-    //pqr3stu8vwx
-    //a1b2c3d4e5f
-    //treb7uchet
-
-    // uniquement 2 chiffres sans lettres : "12"
-    // 2 chiffres => "1c2" on concatène en ignorant les lettres
-    // plus de chiffres => on concatène uniquement le 1er et le dernier
-    // un seul chiffre => on le concatène avec lui même
+    static final List<String> INPUT = Arrays.asList(
+            "1abc2",
+            "pqr3stu8vwx",
+            "a1b2c3d4e5f",
+            "treb7uchet");
 
     @Test
-    void dfsgsdfg() {
-        int result = day1Line("12");
-        assertThat(result).isEqualTo(12);
+    void dfqshfh() {
+        int result = mlkjmlk(INPUT);
+        assertThat(result).isEqualTo(142);
     }
 
-    @Test
-    void dsfhgqhjfdsj() {
-        int result = day1Line("1abc3");
-        assertThat(result).isEqualTo(13);
+    private int mlkjmlk(List<String> input) {
+        return 0;
     }
 
-    private int day1Line(String number) {
-        var stringNumber = number.chars()
-                .mapToObj((int c) -> Character.valueOf((char) c))
+    @ParameterizedTest
+    @CsvSource( {
+            "12,12",
+            "1abc3,13",
+            "pqr3stu8vwx,38",
+            "a1b2c3d4e5f,15",
+            "treb7uchet,77",
+    })
+    void each_line_retuns_its_calibration_value(String line, int expectedCalibrationValue) {
+        int result = calibrationValueFor(line);
+        assertThat(result).isEqualTo(expectedCalibrationValue);
+    }
+
+    private int calibrationValueFor(String line) {
+        var stringNumber = line.chars()
+                .mapToObj((int c) -> (char) c)
                 .filter(Character::isDigit)
                 .map(String::valueOf)
                 .collect(Collectors.joining(""));
-        return Integer.valueOf(stringNumber);
+        String firstDigit = stringNumber.substring(0, 1);
+        String lastDigit = stringNumber.substring(stringNumber.length() -1);
+        return Integer.parseInt(firstDigit + lastDigit);
     }
 
-
     @Test
-    void character_is_a_digit() {
-        assertThat(Character.isDigit('1')).isTrue();
+    void name() throws URISyntaxException, IOException {
+        URL inputFile = this.getClass().getClassLoader().getResource("day1.txt");
+
+        assert inputFile != null;
+        List<String> inputLines = Files.readAllLines(Path.of(inputFile.toURI()));
+
     }
 }
